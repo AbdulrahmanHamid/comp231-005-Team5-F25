e379d2272ac456f106ed7005af9e7a3627c008c0
Md Ripon Hossain <ripon.du.chem@gmail.com>
2025-11-13 17:54:38 -0500

    Integrated the KPI tiles page with db and created chart

--- DENTARA-APP/client/src/pages/Clinic/KPIsView.js
+++ DENTARA-APP/client/src/pages/Clinic/KPIsView.js
@@ -1,24 +1,3 @@
-// import React from "react";
-// import "../../styles/ClinicDashboard.css";
-
-// const KPIsView = () => {
-//   return (
-//     <div className="clinic-page">
-//       <h2>Clinic KPIs Dashboard</h2>
-//       <p>This section will display KPI charts and performance tiles.</p>
-
-//       <div className="clinic-cards">
-//         <div className="card">Total Appointments: 32</div>
-//         <div className="card">Checked-in: 28</div>
-//         <div className="card">Cancelled: 4</div>
-//       </div>
-
-//       <div className="chart-placeholder">
-//         📊 Chart.js integration will appear here.
-//       </div>
-//     </div>
-//   );
-// };
 
 // export default KPIsView;
 import React, { useEffect, useState, useRef } from "react";
